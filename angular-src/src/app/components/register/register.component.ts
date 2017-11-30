f7635ab9067628089dac6e788df456ecf9dd0585
AndrewWaller <awalldrew2@gmail.com>
2017-11-29 19:55:07 -0800

    added background image and particles to most pages

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -15,6 +15,11 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
   constructor(
     private validateService: ValidateService,
     private authService:AuthService,
@@ -22,6 +27,127 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+  };
+
+    this.myParams = {
+        "particles": {
+            "number": {
+            "value": 250,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+            },
+            "color": {
+            "value": "#ffffff"
+            },
+            "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "https://previews.123rf.com/images/asmati/asmati1602/asmati160203108/52184070-Human-ear-sign-Flat-style-icon-on-transparent-background-Stock-Vector.jpg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 2,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "window",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "grab"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 200,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 147.11892112791185,
+            "size": 16.346546791990203,
+            "duration": 1.0625255414793633,
+            "opacity": 0.5,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 179.81201471189226,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+  };
   }
 
   onRegisterSubmit(){
