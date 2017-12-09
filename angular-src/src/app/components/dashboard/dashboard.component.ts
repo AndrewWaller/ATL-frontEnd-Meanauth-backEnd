1a5915b482a53ddbbb5da6f5f67967c6ee2398de
J.R. Ruggiero <fleury14@comcast.net>
2017-12-09 14:44:24 -0800

    button now asks for password, auth service checks it, and if its correct, displays tips

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -23,4 +23,29 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  public viewTips(currentUserName) {
+    const inputPass = prompt('Input your password:');
+    const tempUser = {
+      username: currentUserName,
+      password: inputPass
+    };
+
+    this.authService.authenticateUser(tempUser).subscribe(data => {
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        /*this.flashMessage.show('You are now logged in', {
+          cssClass: 'alert-success',
+          timeout: 5000});*/
+        alert('Password Correct');
+        this.canViewTips = true;
+      } else {
+        /*this.flashMessage.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeout: 5000});*/
+        alert('Password Incorrect');
+      }
+    });
+
+  } // end viewtips
+
 }
